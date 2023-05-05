06e7a4fe03b56ab4715eacdee7e2511fe8f1ed36
orjithedev <godspraise.orji@binghamuni.edu.ng>
2023-05-05 13:34:22 +0100

    new file

---
+++ OOP/src/Math.java
@@ -0,0 +1,16 @@
+public class Maths {
+
+    float pi = 22/7f;
+    private float pi = 22/7f;
+
+    public float getPi() {
+        return pi;
+    }
+
+    public void setPi(float pi) {
+        this.pi = pi;
+    }
+
+    float multiply(float numberOne, float numberTwo){
+        return numberOne * numberTwo;
+    }
